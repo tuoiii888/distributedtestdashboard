0d4b1ddaa68fb7f26d6150d19ee8c2d4c6d26021
tuoiii888 <marcohillspun0suy@hotmail.com>
2024-05-08 20:47:27 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
+}

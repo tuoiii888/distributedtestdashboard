b5d4a58fe3cb3d1d9f70b79c84d57831bed6c828
tuoiii888 <marcohillspun0suy@hotmail.com>
2024-05-10 21:01:52 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+const pull = (arr, ...args) => {
+  let argState = Array.isArray(args[0]) ? args[0] : args;
+  let pulled = arr.filter((v, i) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+};

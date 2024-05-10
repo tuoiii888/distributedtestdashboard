e5a2ead1a8fb1c6366cadd83039f012b223892f9
tuoiii888 <marcohillspun0suy@hotmail.com>
2024-05-10 07:13:33 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function minDepth(root) {
-  if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }

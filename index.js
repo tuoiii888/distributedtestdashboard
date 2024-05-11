57940663f38f1e9990cbc07a88d071ff03f93143
tuoiii888 <marcohillspun0suy@hotmail.com>
2024-05-12 04:24:20 +0700

    test project

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const pull = (arr, ...args) => {
-  let argState = Array.isArray(args[0]) ? args[0] : args;
-  let pulled = arr.filter((v, i) => !argState.includes(v));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-};
+function countVowels(str) {
+  return str.match(/[aeiou]/gi).length;
+}

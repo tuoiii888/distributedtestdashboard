30b57d8534d2b0d87d9206cdbbf3e018212e3b92
tuoiii888 <marcohillspun0suy@hotmail.com>
2024-05-12 10:09:07 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
-}
+const originalObject = { name: "Alice", age: 30 };
+const clonedObject = JSON.parse(JSON.stringify(originalObject));
+console.log(clonedObject); // { name: "Alice", age: 30 }
